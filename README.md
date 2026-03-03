7b8bb9a4880aea4f3eeef6ab4c1de723b60ca6c0
laurent bernabé <laurent.bernabe@gmail.com>
2026-03-03 17:36:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chess_exercises_notes
+
+Syncronize your chess exercises books answers across your devices.
---
+++ android/app/src/main/kotlin/com/loloof64/chess_exercises_notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.loloof64.chess_exercises_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
