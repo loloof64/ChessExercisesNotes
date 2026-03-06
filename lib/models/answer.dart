9e0f9be99dae676a9ede1587db31dc522407e830
laurent bernabé <laurent.bernabe@gmail.com>
2026-03-06 17:11:33 +0100

    feat: we can create an exercise

--- lib/models/answer.dart
+++ lib/models/answer.dart
@@ -1,6 +1,17 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
 class Answer {
   Answer({required this.title, required this.content});
 
   final String title;
   final String content;
+
+  Future<void> serializeToFile(Directory baseDirectory, String fileName) async {
+    final path = Directory(p.join(baseDirectory.path, fileName)).path;
+    final file = File(path);
+
+    await file.create();
+    await file.writeAsString(content);
+  }
 }
--- lib/pages/answers.dart
+++ lib/pages/answers.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chess_exercises_notes/models/answer.dart';
+import 'package:chess_exercises_notes/pages/widgets/dialog_buttons.dart';
 import 'package:chess_exercises_notes/utils/filesystem.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/widgets/i18n_text.dart';
@@ -29,6 +30,162 @@ class AnswerPageWidget extends StatefulWidget {
 class _AnswerPageWidgetState extends State<AnswerPageWidget> {
   List<Answer> _answers = <Answer>[];
   bool _isLoading = false;
+  final TextEditingController _newAnswerNameController =
+      TextEditingController();
+  final TextEditingController _newAnswerContentController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _newAnswerContentController.dispose();
+    _newAnswerNameController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _purposeAddAnswer() async {
+    final (Answer, String)? answerToCreate = await _showAddAnswerDialog();
+    if (answerToCreate == null) return;
+
+    final Directory appSupportDir = await getApplicationSupportDirectory();
+    final Directory booksDir = Directory(
+      p.join(appSupportDir.path, booksRootFolderName),
+    );
+    await booksDir.create();
+
+    final chaptersDir = Directory(p.join(booksDir.path, widget.bookFolderName));
+    await chaptersDir.create();
+
+    final Directory exercisesFolder = Directory(
+      p.join(chaptersDir.path, widget.chapterFolderName),
+    );
+    await exercisesFolder.create();
+
+    await answerToCreate.$1.serializeToFile(exercisesFolder, answerToCreate.$2);
+
+    await _refreshFolderItems();
+  }
+
+  /// Shows the add answer dialog.
+  /// Return : (the new answer, the file name) (Answer, String)?
+  Future<(Answer, String)?> _showAddAnswerDialog() async {
+    return await showDialog<(Answer, String)>(
+      context: context,
+      builder: (dialogContext) {
+        return AlertDialog(
+          title: I18nText("pages.answers.dialogs.add_answer.title"),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 5.0,
+            children: [
+              Row(
+                spacing: 5.0,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  I18nText("pages.answers.dialogs.add_answer.label_name"),
+                  Expanded(
+                    child: TextField(
+                      controller: _newAnswerNameController,
+                      decoration: InputDecoration(
+                        hint: I18nText(
+                          "pages.answers.dialogs.add_answer.placeholder_name",
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                spacing: 5.0,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  I18nText("pages.answers.dialogs.add_answer.label_content"),
+                  Expanded(
+                    child: TextField(
+                      controller: _newAnswerContentController,
+                      minLines: 5,
+                      maxLines: 8,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            CancelButton(
+              onPressed: () {
+                Navigator.of(dialogContext).pop(null);
+              },
+            ),
+            OkButton(
+              onPressed: () async {
+                final purposedNewName = _newAnswerNameController.text;
+                final usedNewName = purposedNewName.endsWith(".txt")
+                    ? purposedNewName
+                    : "$purposedNewName.txt";
+                final securedAnswerName = secureFolderName(usedNewName);
+                final isFolderUnique = !await _isFileNameReserved(
+                  securedAnswerName,
+                );
+
+                if (!dialogContext.mounted) return;
+
+                // Do not test on usedNewName !
+                if (purposedNewName.isEmpty) {
+                  ScaffoldMessenger.of(dialogContext).showSnackBar(
+                    SnackBar(
+                      content: I18nText(
+                        "pages.chapters.dialogs.add_chapter.snack_errors.empty_name",
+                      ),
+                    ),
+                  );
+                  return;
+                } else if (isFolderUnique) {
+                  _newAnswerNameController.clear();
+                  final createdAnswer = Answer(
+                    title: _newAnswerNameController.text,
+                    content: _newAnswerContentController.text,
+                  );
+                  Navigator.of(dialogContext).pop((createdAnswer, usedNewName));
+                } else {
+                  ScaffoldMessenger.of(dialogContext).showSnackBar(
+                    SnackBar(
+                      content: I18nText(
+                        "pages.books.dialogs.snack_errors.already_used_name",
+                      ),
+                    ),
+                  );
+                  return;
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool> _isFileNameReserved(String fileName) async {
+    final Directory appSupportDir = await getApplicationSupportDirectory();
+    final Directory booksDir = Directory(
+      p.join(appSupportDir.path, booksRootFolderName),
+    );
+    await booksDir.create();
+
+    final chaptersDir = Directory(p.join(booksDir.path, widget.bookFolderName));
+    await chaptersDir.create();
+
+    final exercisesDir = Directory(
+      p.join(chaptersDir.path, widget.chapterFolderName),
+    );
+    await exercisesDir.create();
+
+    final children = await listSubdirectoryNames(exercisesDir);
+    return children.contains(fileName);
+  }
 
   Future<void> _refreshFolderItems() async {
     setState(() {
@@ -66,6 +223,7 @@ class _AnswerPageWidgetState extends State<AnswerPageWidget> {
         newAnswers.add(Answer(title: simpleName, content: fileContent));
       }
     }
+    newAnswers.sort();
 
     setState(() {
       _answers = newAnswers;
@@ -181,6 +339,11 @@ class _AnswerPageWidgetState extends State<AnswerPageWidget> {
                 ],
               ),
             ),
+      floatingActionButton: IconButton.outlined(
+        color: Colors.lightGreen,
+        onPressed: _purposeAddAnswer,
+        icon: Icon(Icons.add),
+      ),
     );
   }
 }
